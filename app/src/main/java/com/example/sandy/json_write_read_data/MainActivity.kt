7474fc9f623154fc0e6f212d048d4970c9313e0b
sandeeptile <sandeeptile@gmail.com>
2018-08-10 17:14:24 +0530

    first commit

---
+++ app/src/main/java/com/example/sandy/json_write_read_data/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sandy.json_write_read_data
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
